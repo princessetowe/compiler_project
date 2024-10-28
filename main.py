c64f8e55c70ed4705b94a4740bf2f3707d6da7b3
princessetowe <princessetowe@gmail.com>
2024-10-28 09:44:54 +0100

    second commit

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 from tkinter import *
+import tkinter as tk
 from tkinter.filedialog import asksaveasfilename
+import tkinter.scrolledtext as scrolledtext
 import subprocess
-compiler = Tk()
+compiler = tk.Tk()
 
 compiler.title('Group 11')
 
-def run():
-    command = 'python {file}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output, error = process.communicate()
-    code_output.insert('1.0', output)
-    code_output.insert('1.0', error)
-    code = editor.get('1.0', END)
-    # print(code)
+# def run():
+#     command = 'python {file}'
+#     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+#     output, error = process.communicate()
+#     code_output.insert('1.0', output)
+#     code_output.insert('1.0', error)
+#     code = editor.get('1.0', END)
+#     # print(code)
     # exec(code)
     
 def save_as():
     path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+    if not path.endswith(".py"):
+    #if the file is not a python file then make it a python file
+        path += ".py"
     with open(path, 'w') as file:
          code = editor.get('1.0', END)
          file.write(code)
-    
+
+def open_file(path):
+    try:
+        with open(path, "r") as file:
+            name = file.read()
+            return name
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print("An error occured")
+
 menub = Menu(compiler)
 
-runb = Menu(menub, tearoff=0)
-runb.add_command(label='Run', command=run)
-menub.add_cascade(label='Run', menu=runb)
-compiler.config(menu=menub)
+# runb = Menu(menub, tearoff=0)
+# runb.add_command(label='Run', command=run)
+# menub.add_cascade(label='Run', menu=runb)
+# compiler.config(menu=menub)
+
+text_widget = scrolledtext.ScrolledText(compiler, wrap=tk.WORD)
+text_widget.pack(expand=True, fill='both')
+text_widget.configure(font=("TkDefaultFont", 10))
 
 fileb = Menu(menub, tearoff=0)
-fileb.add_command(label='Open', command=run)
+fileb.add_command(label='Open', command=open_file)
 fileb.add_command(label='Exit', command=exit)
 fileb.add_command(label='Save As', command=save_as)
 menub.add_cascade(label='File', menu=fileb)
---
+++ parser.py
@@ -0,0 +1,10 @@
+class Point:
+    def construct(self,x,y):
+        self.x = x
+        self.y =y
+    
+    def calc():
+        return x + y
+    
+    p = construct(10, 20)
+    p.calc()
